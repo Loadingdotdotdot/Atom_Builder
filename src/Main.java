b2023f722600bfc21c2abdc623ed88653f35b7ab
Loadingdotdotdot <sv072002@gmail.com>
2017-08-04 13:10:47 -0400

    update with fxml

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
 /**
  * Created by Srinath on 7/16/2017.
  */
-public class Main {
+public class Main extends Application {
+
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
 
     static int protons = 10;
 
     public static void main(String[] args) {
-
+        launch(args);
 
 
         Element element = new Element();
