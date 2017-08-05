5890f49eebfd0350b372d97030b2f157e707cc1c
Loadingdotdotdot <sv072002@gmail.com>
2017-08-05 09:00:27 -0400

    update with fxml

--- src/SceneController.java
+++ src/SceneController.java
@@ -1,2 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.input.DragEvent;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
 public class SceneController {
+
+    @FXML
+    public Rectangle area;
+
+    @FXML
+    private void onAreaEntered(DragEvent event) {
+        area.setFill(Paint.valueOf("3392ff50"));
+    }
+
 }
